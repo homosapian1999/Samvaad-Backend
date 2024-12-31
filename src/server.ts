e221e3e424c92ed28cf943dab8f2d6d84340e10d
homosapian <ankitd349@gmail.com>
2024-12-31 23:50:54 +0530

    Initial Implementation of the server for the app

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = parseInt(process.env.PORT as string, 10);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
