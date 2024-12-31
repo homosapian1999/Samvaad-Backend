cb6d9e03f9a4cabe0f1fc65d1365821d5b45ed1c
Ankit Dubey <52074379+homosapian1999@users.noreply.github.com>
2025-01-01 01:05:35 +0530

    Revert "Add testing environment for the server"

--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-if (process.env.NODE_ENV !== "test") {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-}
-
-export default app;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
